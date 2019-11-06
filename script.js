dd87cdd0ae31d19a06bfb2724af9915bc449e971
Anastasiya <36969439+radvilovich@users.noreply.github.com>
2019-11-06 19:53:55 +0300

    feat: generation workarea, html  is empty

--- script.js
+++ script.js
@@ -30,15 +30,16 @@ class Button {
 }
 
 class Keyboard {
+    //config symbols for buttons of keyboard
     config = [
         [['BackQuote', 'ё', 'Ё','`','~'], ['Digit1','1','!','1','!'], ['Digit2','2','"','2','@'], ['Digit3','3','№','3','#'], ['Digit4','4',';','4','$'], ['Digit5','5','%','5','%'], ['Digit6','6',':','6','^'], ['Digit7','7','?','7','&'], ['Digit8','8','*','8','*'], ['Digit9','9','(','9','('], ['Digit0','0',')','0',')'], ['Minus','-','_','-','_'], ['Equal','=','+','=','+'], ['Backspace','Backspace','Backspace','Backspace','Backspace', 90]],
-        [['Tab', 'Tab', 'Tab','Tab','Tab'], ['KeyQ','й','Й','q','Q'], ['KeyW','ц','Ц','w','W'], ['KeyE','у','У','e','E'], ['KeyR','к','К','r','R'], ['KeyT','е','Е','t','T'], ['KeyY','н','Н','y','Y'], ['KeyU','г','Г','u','U'], ['KeyI','ш','Ш','i','I'], ['KeyO','щ','Щ','o','O'], ['KeyP','з','З','p','P'], ['BracketLeft','х','Х','[','{'], ['BracketRight','ъ','Ъ',']','}'], ['Backslash', '\\', '/', '\\', '|'], ['Delete', 'Delete', 'Delete', 'Delete', 'Delete']],
-        [['CapsLock', 'CapsLock', 'CapsLock','CapsLock','CapsLock', 70], ['KeyA','й','Й','a','A'], ['KeyS','ц','Ц','s','S'], ['KeyD','у','У','d','D'], ['KeyF','к','К','f','F'], ['KeyG','е','Е','g','G'], ['KeyH','н','Н','h','H'], ['KeyJ','г','Г','j','J'], ['KeyK','ш','Ш','k','K'], ['KeyL','щ','Щ','l','L'], ['Semicolon','ж','Ж',';',':'], ['Quote','э','Э','\'','"'], ['Enter', 'Enter', 'Enter', 'Enter', 'Enter', 110]],
-        [['ShiftLeft', 'ShiftLeft', 'ShiftLeft','ShiftLeft','ShiftLeft', 90], ['KeyZ','я','Я','z','Z'], ['KeyX','ч','Ч','x','X'], ['KeyC','с','С','c','C'], ['KeyV','м','М','v','V'], ['KeyB','и','И','b','B'], ['KeyN','т','Т','n','N'], ['KeyM','ь','Ь','m','M'], ['Comma','б','Б',',','<'], ['Period','ю','Ю','.','>'], ['Slash','.',',','/','?'], ['RowTop','RowTop','RowTop','RowTop','RowTop'], ['ShiftRight', 'ShiftRight', 'ShiftRight', 'ShiftRight', 'ShiftRight', 90]],
-        [['ControlLeft', 'ControlLeft', 'ControlLeft','ControlLeft','ControlLeft', 65], ['Win','Win','Win','Win','Win'], ['AltLeft','AltLeft','AltLeft','AltLeft','AltLeft'], ['Space','Space','Space','Space','Space', 315], , ['AltRight','AltRight','AltRight','AltRight','AltRight'], ['RowLeft','RowLeft','RowLeft','RowLeft','RowLeft'], ['RowDown','RowDown','RowDown','RowDown','RowDown'], ['RowRight','RowRight','RowRight','RowRight','RowRight'], ['ControlRight','ControlRight','ControlRight','ControlRight','ControlRight']]
+        [['Tab', 'Tab', 'Tab','Tab','Tab'], ['KeyQ','й','Й','q','Q'], ['KeyW','ц','Ц','w','W'], ['KeyE','у','У','e','E'], ['KeyR','к','К','r','R'], ['KeyT','е','Е','t','T'], ['KeyY','н','Н','y','Y'], ['KeyU','г','Г','u','U'], ['KeyI','ш','Ш','i','I'], ['KeyO','щ','Щ','o','O'], ['KeyP','з','З','p','P'], ['BracketLeft','х','Х','[','{'], ['BracketRight','ъ','Ъ',']','}'], ['Backslash', '\\', '/', '\\', '|'], ['Delete', 'Del', 'Del', 'Del', 'Del']],
+        [['CapsLock', 'CapsLk', 'CapsLk','CapsLk','CapsLk', 80], ['KeyA','й','Й','a','A'], ['KeyS','ц','Ц','s','S'], ['KeyD','у','У','d','D'], ['KeyF','к','К','f','F'], ['KeyG','е','Е','g','G'], ['KeyH','н','Н','h','H'], ['KeyJ','г','Г','j','J'], ['KeyK','ш','Ш','k','K'], ['KeyL','щ','Щ','l','L'], ['Semicolon','ж','Ж',';',':'], ['Quote','э','Э','\'','"'], ['Enter', 'Enter', 'Enter', 'Enter', 'Enter', 100]],
+        [['ShiftLeft', 'Shift', 'Shift','Shift','Shift', 90], ['KeyZ','я','Я','z','Z'], ['KeyX','ч','Ч','x','X'], ['KeyC','с','С','c','C'], ['KeyV','м','М','v','V'], ['KeyB','и','И','b','B'], ['KeyN','т','Т','n','N'], ['KeyM','ь','Ь','m','M'], ['Comma','б','Б',',','<'], ['Period','ю','Ю','.','>'], ['Slash','.',',','/','?'], ['RowTop','▲','▲','▲','▲'], ['ShiftRight', 'Shift', 'Shift', 'Shift', 'Shift', 90]],
+        [['ControlLeft', 'Ctrl', 'Ctrl','Ctrl','Ctrl', 65], ['Win','Win','Win','Win','Win'], ['AltLeft','Alt','Alt','Alt','Alt'], ['Space',' ',' ',' ',' ', 315], , ['AltRight','Alt','Alt','Alt','Alt'], ['RowLeft','◄','◄','◄','◄'], ['RowDown','▼','▼','▼','▼'], ['RowRight','►','►','►','►'], ['ControlRight','Ctrl','Ctrl','Ctrl','Ctrl']]
     ];
 
-    elementTextarea;
+    elementTextarea; // DOM-Element #textarea
     constructor(elementKeyboard, elementTextarea) {
         this.elementTextarea = elementTextarea;
         console.log(this.elementTextarea);
@@ -62,7 +63,31 @@ class Keyboard {
     }
 }
 
-new Keyboard(document.getElementById("keyboard"), document.getElementById("textarea"));
+// generation elements
+class WorkArea {
+    constructor(body) {
+        const textareaWrapper = document.createElement('div');
+        body.append(textareaWrapper);
+        const textarea = document.createElement('div');
+        body.append(textarea);
+        textarea.className = 'textarea';
+
+        const keyboardWrapper = document.createElement('div');
+        body.append(keyboardWrapper);
+        const keyboard = document.createElement('div');
+        body.append(keyboard);
+        keyboard.className = 'keyboard';
+
+        new Keyboard(keyboard, textarea); //create instance of classes Keyboard
+
+    }
+} 
+
+const body = document.getElementById("body"); // get DOM-Element #body
+const elementKeyboard = document.getElementById("keyboard"); // get DOM-Element #keyboard
+const elementTextarea = document.getElementById("textarea"); // get DOM-Element #textarea
+//new Keyboard(elementKeyboard, elementTextarea); //create instance of classes Keyboard
+new WorkArea(body); //create instance of class WorkArea
 
 
 
