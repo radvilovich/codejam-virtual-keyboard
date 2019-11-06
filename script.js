d67f58558331407a58e51c8a40f06c8987e2c47c
Anastasiya <36969439+radvilovich@users.noreply.github.com>
2019-11-06 22:49:10 +0300

    feat: special buttons were excluded

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ class Button {
     }
 
     setElementParams() {
-        this.buttonElement.innerHTML = this.buttonConfig[1];
+        this.buttonElement.innerHTML = this.buttonConfig['ru'];
         this.buttonElement.className = 'button'; 
         
-        if(this.buttonConfig[5]) {
-            this.buttonElement.style.width = this.buttonConfig[5] + 'px';
+        if(this.buttonConfig['width']) {
+            this.buttonElement.style.width = this.buttonConfig['width'] + 'px';
         }
     }
 
@@ -25,10 +25,14 @@ class Button {
     events() {
         this.buttonElement.addEventListener( "click" , () => {
             this.callback(this.buttonConfig);
+            this.buttonElement.className = 'button round';
+            setTimeout(() => {
+                this.buttonElement.className = 'button';
+            }, 200);
         });
 
         document.addEventListener('keydown', (event) => {
-            if (event.code === this.buttonConfig[0])  {
+            if (event.code === this.buttonConfig['key'])  {
                 console.log(this.buttonElement);
                 this.buttonElement.className = 'button active';
 
@@ -37,15 +41,6 @@ class Button {
                 }, 200);
             }
         });
-
-        this.buttonElement.addEventListener('click', (event) => {
-            this.buttonElement.className = 'button round';
-            setTimeout(() => {
-                this.buttonElement.className = 'button';
-            }, 200);
-        });
-
-        
     }
 }
 
@@ -59,6 +54,21 @@ class Keyboard {
         [['ControlLeft', 'Ctrl', 'Ctrl','Ctrl','Ctrl', 65], ['Win','Win','Win','Win','Win'], ['AltLeft','Alt','Alt','Alt','Alt'], ['Space',' ',' ',' ',' ', 315], , ['AltRight','Alt','Alt','Alt','Alt'], ['RowLeft','◄','◄','◄','◄'], ['RowDown','▼','▼','▼','▼'], ['RowRight','►','►','►','►'], ['ControlRight','Ctrl','Ctrl','Ctrl','Ctrl']]
     ];
 
+    buttonsWithoutValue = [
+        'Tab', 
+        'CapsLock', 
+        'ShiftLeft', 
+        'ControlLeft', 
+        'Win', 
+        'AltLeft',  
+        'AltRight', 
+        'ControlRight',
+        'ShiftRight',
+        'Enter',
+        'Delete',
+        'Backspace'
+    ];
+
     elementTextarea; // DOM-Element #textarea
     constructor(elementKeyboard, elementTextarea) {
         this.elementTextarea = elementTextarea;
@@ -69,18 +79,38 @@ class Keyboard {
             row.className = 'row';
 
             rowConfig.forEach((buttonConfig) => {
-                const button = new Button(buttonConfig, this.changeInputValue);
+                const newButtonConfig = this.changeButtonConfig(buttonConfig);
+
+                const button = new Button(newButtonConfig, this.changeInputValue);
                 row.append(button.getButtonElement());
             })
         })
         console.log(elementKeyboard);
+    }
+
+    changeButtonConfig(buttonConfig) {
+        let newConfig = {
+            key: buttonConfig[0],
+            ru: buttonConfig[1],
+            RU: buttonConfig[2],
+            en: buttonConfig[3],
+            EN: buttonConfig[4],
+            width: buttonConfig[5]
+        };
+
+        if(this.buttonsWithoutValue.includes(buttonConfig[0])) {
+            newConfig.text = buttonConfig[1];
+        }
+
+        return newConfig;
 
     }
 
     changeInputValue = (buttonConfig) => {
-        this.elementTextarea.value += buttonConfig[2];
+        if (!buttonConfig.text) {
+            this.elementTextarea.value += buttonConfig['ru'];
+        }
         console.log(this.elementTextarea);
-        
     }
 
     
